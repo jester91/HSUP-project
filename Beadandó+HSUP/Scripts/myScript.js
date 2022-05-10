c517ecf847952bf1758d425d829a86ebf19a3705
VSZA <vszandras@gmail.com>
2022-05-10 16:25:58 +0200

    Questions page upgrade (buttons, content)

--- Beadandó+HSUP/Scripts/myScript.js
+++ Beadandó+HSUP/Scripts/myScript.js
@@ -7,7 +7,7 @@ function menu() {
   }
 }
 
-//Smooth scroll for the dedicated section of the site
+//Smooth scroll for the dedicated section of the site, and activ nav bar back color
 
 function FunctionProduct() {
   var TargetArticle = document.getElementById("HomeArticle2");
@@ -44,5 +44,58 @@ function FunctionContact() {
   document.getElementById('NavContact').style.backgroundColor ='#00e091';
 }
 
+//Question page "next" and "back" button event
+function question1next(){
+  document.getElementById('question1').style.display = 'none';
+  document.getElementById('question2').style.display = 'flex';
+}
 
+function question2next(){
+  document.getElementById('question2').style.display = 'none';
+  document.getElementById('question3').style.display = 'flex';
+}
+
+function question2back(){
+  document.getElementById('question2').style.display = 'none';
+  document.getElementById('question1').style.display = 'flex';
+}
+
+function question3next(){
+  document.getElementById('question3').style.display = 'none';
+  document.getElementById('question4').style.display = 'flex';
+}
+
+function question3back(){
+  document.getElementById('question3').style.display = 'none';
+  document.getElementById('question2').style.display = 'flex';
+}
+
+function question4next(){
+  document.getElementById('question4').style.display = 'none';
+  document.getElementById('question5').style.display = 'flex';
+}
+
+function question4back(){
+  document.getElementById('question4').style.display = 'none';
+  document.getElementById('question3').style.display = 'flex';
+}
+
+function question5next(){
+  document.getElementById('question5').style.display = 'none';
+  document.getElementById('question6').style.display = 'flex';
+}
+
+function question5back(){
+  document.getElementById('question5').style.display = 'none';
+  document.getElementById('question4').style.display = 'flex';
+}
+//next button need to be changed
+function question6submit(){
+  
+}
+
+function question6back(){
+  document.getElementById('question6').style.display = 'none';
+  document.getElementById('question5').style.display = 'flex';
+}
 
